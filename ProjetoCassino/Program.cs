a59ff955fbce96084febf680526498e4c901cbe6
mrissexavier <m.risse.dev@gmail.com>
2022-03-18 11:37:18 -0300

    implementção jogos

--- ProjetoCassino/Program.cs
+++ ProjetoCassino/Program.cs
@@ -37,6 +37,7 @@
             int op = 1;
             while (op != 0)
             {
+                Console.Clear();
                 Console.WriteLine("Menu Raspadinhas");
                 Console.WriteLine("1-Raspadinha A");
                 Console.WriteLine("2-Raspadinha B");
@@ -65,10 +66,76 @@
 
         private static void RaspadinhaB()
         {
+            Console.Clear();
+
+            int numPrincipal = rnd.Next(1, 10);
+            int premio = rnd.Next(1, 100);
+
+            int num1 = rnd.Next(1, 10);
+            int num2 = rnd.Next(1, 10);
+            int num3 = rnd.Next(1, 10);
+            int num4 = rnd.Next(1, 10);
+            int num5 = rnd.Next(1, 10);
+            int num6 = rnd.Next(1, 10);
+            int num7 = rnd.Next(1, 10);
+            int num8 = rnd.Next(1, 10);
+            int num9 = rnd.Next(1, 10);
+
+            int contador = 0;
+            if (numPrincipal == num1) contador++;
+            if (numPrincipal == num2) contador++;
+            if (numPrincipal == num3) contador++;
+            if (numPrincipal == num4) contador++;
+            if (numPrincipal == num5) contador++;
+            if (numPrincipal == num6) contador++;
+            if (numPrincipal == num7) contador++;
+            if (numPrincipal == num8) contador++;
+            if (numPrincipal == num9) contador++;
+
+            Console.WriteLine($"Número principal: {numPrincipal} (R${premio})");
+            Console.WriteLine($"{num1}  {num2}  {num3}");
+            Console.WriteLine($"{num4}  {num5}  {num6}");
+            Console.WriteLine($"{num7}  {num8}  {num9}");
+
+            if(contador >= 3)
+                Console.WriteLine($"Boa mlk, ganhou R${premio}");
+            else
+                Console.WriteLine("Ops, raspadinha não premiada");
+            
+            Console.ReadKey();
         }
 
         private static void RaspadinhaA()
         {
+            Console.Clear();    
+
+            int numPrincipal = rnd.Next(1, 10);
+            int num1 = rnd.Next(1, 10); int premio1 = rnd.Next(1, 100);
+            int num2 = rnd.Next(1, 10); int premio2 = rnd.Next(1, 100);
+            int num3 = rnd.Next(1, 10); int premio3 = rnd.Next(1, 100);
+            int num4 = rnd.Next(1, 10); int premio4 = rnd.Next(1, 100);
+            int num5 = rnd.Next(1, 10); int premio5 = rnd.Next(1, 100);
+
+            Console.WriteLine($"Número principal: {numPrincipal}");
+            Console.WriteLine($"1º número: {num1} (R${premio1})");
+            Console.WriteLine($"2º número: {num2} (R${premio2})");
+            Console.WriteLine($"3º número: {num3} (R${premio3})");
+            Console.WriteLine($"4º número: {num4} (R${premio4})");
+            Console.WriteLine($"5º número: {num5} (R${premio5})");
+
+            int premio = 0;
+
+            if (num1 == numPrincipal) premio += premio1;
+            if (num2 == numPrincipal) premio += premio2;
+            if (num3 == numPrincipal) premio += premio3;
+            if (num4 == numPrincipal) premio += premio4;
+            if (num5 == numPrincipal) premio += premio5;
+
+            Console.WriteLine($"Premio: R${premio}");
+
+
+            Console.ReadKey();
+
         }
 
         private static void Loteria()
@@ -85,18 +152,19 @@
 
             Console.WriteLine($"Bilhete premiado: {numPremiado}");
 
+
             if (numPremiado == numJogador)
             {
-                Console.WriteLine("Chave mlk, ganhou o 1º prémio!");
+                Console.WriteLine("Chave mlk, ganhou o 1º premio!");
             }
             else if (numPremiado % 1000 == numJogador % 1000)
             {
-                Console.WriteLine("Pica pae, ganhou o 2º prémio!");
+                Console.WriteLine("Pica pae, ganhou o 2º premio!");
 
             }
             else if (numPremiado % 100 == numJogador % 100)
             {
-                Console.WriteLine("Mlk de ouro, ganhou o 3º prémio!");
+                Console.WriteLine("Mlk de ouro, ganhou o 3º premio!");
             }
             else
             {
